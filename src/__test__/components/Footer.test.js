859f6436d04d465fddb85ca95e40ebfc3b48729a
guissmejia <guissmejia@gmail.com>
2020-04-03 14:24:10 -0500

    Footer component test

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer/>', () => {
+  const footer = mount(<Footer />);
+  test('Render del Componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del Título', () => {
+    expect(
+      footer
+        .find('footer')
+        .text()
+        .toEqual('')
+    );
+  });
+});
